702772bb210c1116de5f35437b0679f62afe38c2
Kevin Hwang <k3vinhwang@gmail.com>
2013-11-15 20:08:41 -0800

    Add scoping

--- javascript/scoping.js
+++ javascript/scoping.js
@@ -25,3 +25,29 @@ var z = x.foo.bind(x);
 z(); // returns 5
 
 // Now, time to write bind!
+
+Function.prototype.myBind = function (thisObj) {
+	var fToBind = this;
+	return function () {
+		return fToBind.apply(thisObj)
+	}
+};
+
+var q = x.foo.myBind(x);
+q(); // returns 5
+
+// With arguments
+
+x.baz = function (i) { return this.bar + i };
+
+Function.prototype.myBindWithArgs = function (thisObj) {
+	var fToBind = this;
+	return function () {
+		return fToBind.apply(thisObj, arguments)
+	}
+};
+
+var w = x.baz.myBindWithArgs(x);
+w(1); // returns 6
+
+// Side note: Use this to do partial application! Currying! woo
