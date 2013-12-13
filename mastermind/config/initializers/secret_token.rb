b2e33ead6e312cc3af090272f6c3d82e68f6eda1
Kevin Hwang <k3vinhwang@gmail.com>
2013-12-13 13:38:03 -0800

    Mastermind.

---
+++ mastermind/config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Mastermind::Application.config.secret_key_base = 'dce1521470909b6575d5d6be57b954d294e611931be17536a510919fe06ab9022befcdbc8a8e2959fb35cea292a20568f3ac738e5f539f391250f866b7a6399b'
---
+++ mastermind/config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Mastermind::Application.config.session_store :cookie_store, key: '_mastermind_session'
